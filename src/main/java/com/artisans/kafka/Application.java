12a49377d6bd85016d25b9d59bb5e8930c9ebef5
DAPO <oladapooloyede@gmail.com>
2020-02-07 06:29:05 -0700

    protobuf commit

--- src/main/java/com/artisans/kafka/Application.java
+++ src/main/java/com/artisans/kafka/Application.java
@@ -1,9 +1,12 @@
 package com.artisans.kafka;
 
+import com.users.CustomerRegistrationRequest;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @SpringBootApplication
@@ -20,5 +23,20 @@ public class Application {
             System.out.println("Sending message");
             return "Help!!!";
         };
+    }
+
+    @Bean
+    public Function<KStream<String, CustomerRegistrationRequest>, KStream<String, CustomerRegistrationRequest>>  operat() {
+
+        return s -> s
+                .mapValues( v -> {
+                    return CustomerRegistrationRequest
+                            .newBuilder()
+                            .setFullName(v.getFullName() + "stream")
+                            .setEmail(v.getEmail())
+                            .setPassword(v.getPassword())
+                            .setPhoneNumber(v.getPhoneNumber())
+                            .build();
+                });
     }
 }
